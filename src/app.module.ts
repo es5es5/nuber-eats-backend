b14963924ce36768c646beb493b9cf5ae5eb922a
es5es5 <es5e5s@nate.com>
2022-10-13 15:24:30 +0900

    GraphQL init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurantModule,
+  ],
 })
 export class AppModule {}
